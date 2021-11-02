9e701d58d65e2606ed443d6a29d81b5c169f8ec9
Sergey Mednyh <mednyh@gmail.com>
2021-11-03 06:36:31 +0700

    chang main

--- src/main/java/work/Main.java
+++ src/main/java/work/Main.java
@@ -1,22 +1,13 @@
 package work;
 
-import work.nonCommand.StaticService;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.Date;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 public class Main {
-        private static final ScheduledExecutorService scheduler = Executors
-                .newScheduledThreadPool(1);
+
         public static void main(String[] args) throws TelegramApiException {
+            System.out.println("Bot started!");
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
             telegramBotsApi.registerBot(new MyTelegramBot());
@@ -24,12 +15,4 @@ public class Main {
             e.printStackTrace();
         }
     }
-
-    private static Runnable resetAllResults(){
-        System.out.println("Test scheduler: " + new Date());
-        StaticService.users.clear();
-        return null;
-    }
-
-
 }
