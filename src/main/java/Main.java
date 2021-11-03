e92e42182d70a397b41db1a53e045f5a0f9c297b
Sergey Mednyh <mednyh@gmail.com>
2021-11-03 07:15:38 +0700

    chang Procfile5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,9 @@
-package work;
+//package work;
 
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+import work.MyTelegramBot;
 
 public class Main {
 
